e947468b56e2ee518d482f32269371b258f22e1e
dimmur <przemyslaw.kukulski@gmail.com>
2023-04-06 00:36:20 +0200

    Use single value for weight box width

--- components/WeightDrawBox/WeightDrawBox.tsx
+++ components/WeightDrawBox/WeightDrawBox.tsx
@@ -6,13 +6,13 @@ interface WeightBlockData {
 }
 
 const blocksByWeight: Record<number, WeightBlockData> = {
-    2500: {weight: 25, width: 14, height: 70, css: {fill: 'rgb(255,0,0)', stroke: 'rgb(0,0,0)'}},
-    2000: {weight: 20, width: 14, height: 70, css: {fill: 'rgb(152,152,236)', stroke: 'rgb(0,0,0)'}},
-    1500: {weight: 15, width: 12, height: 60, css: {fill: 'rgb(222,222,222)', stroke: 'rgb(0,0,0)'}},
+    2500: {weight: 25, width: 10, height: 70, css: {fill: 'rgb(255,0,0)', stroke: 'rgb(0,0,0)'}},
+    2000: {weight: 20, width: 10, height: 70, css: {fill: 'rgb(152,152,236)', stroke: 'rgb(0,0,0)'}},
+    1500: {weight: 15, width: 10, height: 60, css: {fill: 'rgb(222,222,222)', stroke: 'rgb(0,0,0)'}},
     1000: {weight: 10, width: 10, height: 50, css: {fill: 'rgb(222,222,222)', stroke: 'rgb(0,0,0)'}},
-    500:  {weight: 5, width: 8, height: 40, css: {fill: 'rgb(222,222,222)', stroke: 'rgb(0,0,0)'}},
-    250:  {weight: 2.5, width: 8, height: 30, css: {fill: 'rgb(0,0,0)', stroke: 'rgb(0,0,0)'}},
-    125:  {weight: 1.25, width: 6, height: 20, css: {fill: 'rgb(222,222,222)', stroke: 'rgb(0,0,0)'}},
+    500:  {weight: 5, width: 10, height: 40, css: {fill: 'rgb(222,222,222)', stroke: 'rgb(0,0,0)'}},
+    250:  {weight: 2.5, width: 10, height: 30, css: {fill: 'rgb(0,0,0)', stroke: 'rgb(0,0,0)'}},
+    125:  {weight: 1.25, width: 10, height: 20, css: {fill: 'rgb(222,222,222)', stroke: 'rgb(0,0,0)'}},
 }
 
 const cssBar = {
